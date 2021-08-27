4e8edbe6c9f7234018a6b9b3b6c0ddb4b71556dc
Ronald Pineda <rspineda@B131LSL.local>
2021-08-28 00:40:37 +0200

    added images that will be used on the app / set image for start and stop recording on main screen

--- Pitch_Perfect/ViewController.swift
+++ Pitch_Perfect/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     @IBAction func recordAudio(_ sender: Any) {
