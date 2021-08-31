110e046cdab020ff41576f2d698f921129cbbde2
Ronald Pineda <rspineda@B131LSL.local>
2021-08-31 13:01:11 +0200

    stop recording button disabled when app runs and enablled again when record button is pressed

--- Pitch_Perfect/ViewController.swift
+++ Pitch_Perfect/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-    
     @IBOutlet weak var recordButton: UIButton!
-    
     @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,13 +24,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed")
+        //print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button was pressed")
+        //print("stop recording button was pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
     
 }
