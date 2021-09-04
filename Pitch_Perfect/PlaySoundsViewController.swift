59e22f22b12255cbe78f0ef9ded8e6878910d83d
Ronald Pineda <rspineda@B131LSL.local>
2021-09-05 01:02:07 +0200

    connected UIButtons for effect sounds to playSundController

--- Pitch_Perfect/PlaySoundsViewController.swift
+++ Pitch_Perfect/PlaySoundsViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
-    //var recordedAudioURL = URL!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
 
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+       print("play sound button pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+       print("atop audio button pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
